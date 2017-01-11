d3ece1b21c2c615468c10ace382310ea39962139
villell <ville.loytynoja@cs.tamk.fi>
2017-01-11 16:22:10 +0000

    Iltakoodailut

--- index.js
+++ index.js
@@ -2,62 +2,69 @@ class TodoApp extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handlePrice = this.handlePrice.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = { items: [], text: '' };
+    this.state = {items: [], text: '', price: ''};
   }
-
+  
   render() {
-    return React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'h3',
-        null,
-        'TODO'
-      ),
-      React.createElement(TodoList, { items: this.state.items }),
-      React.createElement(
-        'form',
-        { onSubmit: this.handleSubmit },
-        React.createElement('input', { onChange: this.handleChange, value: this.state.text }),
-        React.createElement(
-          'button',
-          null,
-          'Add #' + (this.state.items.length + 1)
-        )
-      )
+    return (
+      <div>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <input onChange={this.handlePrice} value={this.state.price} />
+          <button>Lisää</button>
+        </form>
+      </div>
     );
   }
 
   handleChange(e) {
-    this.setState({ text: e.target.value });
+    this.setState({text: e.target.value});
+  }
+  
+  handlePrice(e) {
+    this.setState({price: e.target.value});
   }
 
   handleSubmit(e) {
     e.preventDefault();
     var newItem = {
       text: this.state.text,
+      price: this.state.price,
       id: Date.now()
     };
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       items: prevState.items.concat(newItem),
-      text: ''
+      text: '',
+      price: ''
     }));
   }
 }
 
 class TodoList extends React.Component {
+  
   render() {
-    return React.createElement(
-      'ul',
-      null,
-      this.props.items.map(item => React.createElement(
-        'li',
-        { key: item.id },
-        item.text
-      ))
+    return (
+      <ul className="tuotteet">
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text + " - "+ item.price+"€"}
+            <span onClick={this.remove.bind(this, item)} className="glyphicon glyphicon-remove"></span>
+          </li>
+        ))}
+      </ul>
     );
   }
+  
+  remove(item){
+    const newState = this.state.data;
+    if (newState.indexOf(item) > -1) {
+      newState.splice(newState.indexOf(item), 1);
+      this.setState({data: newState})
+    }
+  }
+  
 }
 
-ReactDOM.render(React.createElement(TodoApp, null), document.getElementById("app"));
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
